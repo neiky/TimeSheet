081dab30b35d99a28c9fb8f82e683e81c9fd8da2
Michael Neike <michi.neiky@googlemail.com>
2013-03-12 22:41:22 +0100

    project views prettyfied

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,6 +24,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.js
       format.json { render json: @project }
     end
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,15 +3,17 @@ class Contact < ActiveRecord::Base
   belongs_to :client
   attr_accessible :comment, :email, :firstname, :name, :phone, :client, :client_id, :user
 
+  default_scope order: 'Contacts.firstname, Contacts.name ASC'
+
   def to_s
-	  if !self.firstname
-			return self.name
-		else
-			if !self.name
-				return self.firstname
-			else
-				return self.firstname + " " + self.name
-			end
-	  end
-	 end
+    if !self.firstname
+      return self.name
+    else
+      if !self.name
+        return self.firstname
+      else
+        return self.firstname + " " + self.name
+      end
+    end
+   end
 end
